b298338ce279566d61fb4ae8c545eab244c492f7
VassiAeLin <115078631+VassiAeLin@users.noreply.github.com>
2023-06-19 01:10:57 +0600

    first commit

---
+++ site/Dop/pract/add_form.php
@@ -0,0 +1,12 @@
+<div id="add_new_photo">
+    <form action="new_photo.php" method="post">
+        <input type="text" id="new_photo_src" placeholder="Фоточку сюды" name="image">
+        <input type="text" id="new_photo_text" placeholder="Сюды подписочку" name="text">
+        <button id="add_photo">Добавить</button>
+        <button id="cancel">Отмена</button>
+    </form>
+</div>
+
+<div id="popup_photo">
+    <img src="" alt="">
+</div>
